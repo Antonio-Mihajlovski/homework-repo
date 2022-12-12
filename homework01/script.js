4c1286f59c1addc49d60f6d35955839924307ca0
Antonio-Mihajlovski <mihajlovski012@gmail.com>
2022-12-12 15:27:04 +0100

    finished homework

---
+++ homework01/script.js
@@ -0,0 +1,7 @@
+let phone = 119.95 ;
+let taxRate ;
+let calculatedPrice ;
+calculatedPrice = phone * 30 ;
+taxRate = calculatedPrice * 0.05 ;
+console.log (calculatedPrice);
+console.log (taxRate);
