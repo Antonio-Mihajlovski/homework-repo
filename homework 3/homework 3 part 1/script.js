d1f6f3b4e5432b240ddc180431025feeb09589a6
Antonio-Mihajlovski <mihajlovski012@gmail.com>
2022-12-16 20:54:18 +0100

    homework 3 part1

---
+++ homework 3/homework 3 part 1/script.js
@@ -0,0 +1,34 @@
+function notDefined(something){
+    let firstExercise;
+    console.log(firstExercise);
+}
+notDefined();
+
+function objects(){
+    let secondExercise = null;
+    console.log(typeof secondExercise);
+}
+objects ();
+
+
+function nummbers() {
+    let thirdExercise = 2;
+    console.log(thirdExercise);
+}
+
+nummbers();
+
+function strings (){
+    let name = "Antonio" + " " + "Mihajlovski"
+    console.log(name);
+}
+strings();
+
+function boolean() {
+    let firstNumber = 1;
+    let secondNumber = 2;
+    let thirdNumber = firstNumber > secondNumber;
+    console.log(thirdNumber);
+}
+
+boolean();
