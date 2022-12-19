2df46e066bba313da5d3e36b75d63e12000ee004
Antonio-Mihajlovski <mihajlovski012@gmail.com>
2022-12-19 20:44:13 +0100

    class 4 homework1

---
+++ homework 4/homework 1/app.js
@@ -0,0 +1,8 @@
+let arrays = ["Antonio" , "happy" , "exercise"];
+function tellStory(){
+        console.log(`Hello my name is ${arrays[0]}.
+        Today im ${arrays[1]}.
+        I finnaly solved this ${arrays[2]} `);
+    }
+
+tellStory(arrays);
