151ac756e6b1d83ef0c6b813cc503a78b588d14a
Antonio-Mihajlovski <mihajlovski012@gmail.com>
2022-12-26 19:59:43 +0100

    fixed homework 3 part 1

--- homework 3/homework 3 part 1/script.js
+++ homework 3/homework 3 part 1/script.js
@@ -1,34 +1,9 @@
 function notDefined(something){
     let firstExercise;
-    console.log(firstExercise);
+    console.log(typeof something);
 }
 notDefined();
-
-function objects(){
-    let secondExercise = null;
-    console.log(typeof secondExercise);
-}
-objects ();
-
-
-function nummbers() {
-    let thirdExercise = 2;
-    console.log(thirdExercise);
-}
-
-nummbers();
-
-function strings (){
-    let name = "Antonio" + " " + "Mihajlovski"
-    console.log(name);
-}
-strings();
-
-function boolean() {
-    let firstNumber = 1;
-    let secondNumber = 2;
-    let thirdNumber = firstNumber > secondNumber;
-    console.log(thirdNumber);
-}
-
-boolean();
+notDefined(null);
+notDefined(2);
+notDefined(`"Antonio" + "Mihajlovski`);
+notDefined(false);
