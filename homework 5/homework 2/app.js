839113d373d4a1b4827a7623af8bdf7ba12380e5
Antonio-Mihajlovski <mihajlovski012@gmail.com>
2022-12-28 19:24:06 +0100

    fixed class 5 homework 2

--- homework 5/homework 2/app.js
+++ homework 5/homework 2/app.js
@@ -1,11 +1,23 @@
-let number = document.getElementsByTagName("li");
-console.log(number);
+let number = document.getElementById("content");
+let sumOfAll = document.getElementById("sum");
 
 let numbers = [1 , 2, 3 ,4 ,5];
-number[0].textContent = numbers[0];
-number[1].textContent = numbers[1];
-number[2].textContent = numbers[2];
-number[3].textContent = numbers[3];
-number[4].textContent = numbers[4];
-number[5].textContent = `${numbers[0]} + ${numbers[1]} + ${numbers[2]} + ${numbers[3]} + ${numbers[4]} = 
-${numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4]}  `;
+function printGrades (array){
+    let num = "";
+    number.innerHTML = "";
+    let sum = 0;
+    liContent = "";
+    
+    for (let i = 0;i < numbers.length; i++){
+        liContent += `<li>${array[i]}</li>`
+        sum += array[i];
+        if (i === array.length - 1){
+            num += array[i] + " =";
+        } else {
+            num += array[i] + " + ";
+        }
+    }
+    content.innerHTML += `<ul> ${liContent} </ul>`;
+    sumOfAll.innerHTML += `${num} ${sum}`
+}
+printGrades(numbers);
